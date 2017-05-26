ec1fa2f261c98513c3a67b332141bef2742505d8
Paul Vollmer <paul.vollmer@protonmail.com>
2017-05-26 17:20:53 +0200

    create path class

---
+++ src/path.js
@@ -0,0 +1,18 @@
+/**
+ * Swagger path utility
+ */
+class Path {
+  constructor (data) {
+    this.data = data
+  }
+
+  hasPath(name) {
+
+  }
+
+  walkMethods() {
+
+  }
+}
+
+module.exports = Path
